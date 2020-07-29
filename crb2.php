2dc7921cd60acfbc6b904205e0f02d24205b25b3
Niyi Enny <myjessy.sr@gmail.com>
2020-07-29 12:39:50 +0100

    Revert "Update crb2.php"
    This reverts commit cc1d5e0df92ea9469d3a4092a7efae344c1d2081.

--- crb2.php
+++ crb2.php
@@ -4,7 +4,7 @@
     $do = new All($connect);
     $datenow = date('Y-m-d', strtotime('now'));
     $branch = $_POST['branch'];
-    $lets = "SELECT id, crbnumber FROM crbs WHERE branch = '$branch' AND datee = '$datenow' AND category NOT REGEXP '^Offline' ORDER BY id DESC LIMIT 1";
+    $lets = "SELECT id, crbnumber FROM crbs WHERE branch = '$branch' ORDER BY id DESC LIMIT 1";
     $get = mysqli_query($connect, $lets);
     $getData = $_POST['fi'];
     $fetch = preg_split("/\,/", $getData);
