0f7a9757bf4d09e7bd401c28af214b14285eb5fb
Niyi Enny <myjessy.sr@gmail.com>
2020-08-09 07:10:17 +0100

    Removed Offline Button

--- crbHome.php
+++ crbHome.php
@@ -291,7 +291,7 @@ function printContent(el){
                 <button class="btn btn-outline-primary" data-toggle="modal" data-target="#cusReg">Register New Customer</button>
 
                 <button class="btn btn-outline-info" data-toggle="modal" data-target="#report">My report for today</button>
-                <button class="btn btn-outline-danger" > <a href="crbHome2.php" style="text-decoration: none; ">Upload Offline sales</a> </button>
+            <!--    <button class="btn btn-outline-danger" > <a href="crbHome2.php" style="text-decoration: none; ">Upload Offline sales</a> </button> -->
                 </div>
                     <h5 class="text-primary mt-2" align="center"><?php echo $status ?></h5>
 
