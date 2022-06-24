41d9e84e43be6f3a25cffa9dcbd30469e721258b
Niyi Enny <myjessy.sr@gmail.com>
2022-06-24 13:35:29 +0100

    Update all.php

--- classes/all.php
+++ classes/all.php
@@ -2,7 +2,7 @@
     
     error_reporting (E_ALL ^ E_NOTICE);
     
-  $connect = mysqli_connect('localhost', 'root', 'derogas', 'dero');
+  $connect = mysqli_connect('localhost', 'root', 'aicogas', 'dero');
     
     // if(!$connect){
     
